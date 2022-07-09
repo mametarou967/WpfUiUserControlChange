50e7214755d3acf8d428ed4e4082640421f8db91
mametarou967 <mametarou967@gmail.com>
2022-07-10 07:52:45 +0900

    initial commit

---
+++ WpfCardReaderScreen/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using WpfCardReaderScreen.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace WpfCardReaderScreen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ViewA>();
+            containerRegistry.RegisterForNavigation<ViewB>();
+
+        }
+    }
+}
---
+++ WpfCardReaderScreen/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace WpfCardReaderScreen.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public IRegionManager RegionManager { get; }
+        public DelegateCommand<string> ShowViewCommand { get; }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            this.RegionManager = regionManager;
+            this.ShowViewCommand = new DelegateCommand<string>(this.ShowView);
+        }
+
+        public void ShowView(string viewName)
+        {
+            this.RegionManager.RequestNavigate("ContentRegion", viewName);
+        }
+    }
+}
