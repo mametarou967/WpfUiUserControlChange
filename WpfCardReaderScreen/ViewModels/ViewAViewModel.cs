8cc100fdf36f43d942d58f730f93c129fae04d12
mametarou967 <mametarou967@gmail.com>
2022-07-10 13:53:28 +0900

    画面の切り替えと操作の追加

---
+++ WpfCardReaderScreen/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace WpfCardReaderScreen.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        public IRegionManager RegionManager { get; }
+        public DelegateCommand ShiyouCommand { get; }
+
+        public ViewAViewModel(IRegionManager regionManager)
+        {
+            this.RegionManager = regionManager;
+            this.ShiyouCommand = new DelegateCommand(this.Shiyou);
+        }
+
+        public void Shiyou()
+        {
+            this.RegionManager.RequestNavigate("ContentRegion", "ViewB");
+        }
+    }
+}
